8623dacb30064f473451fddc3363bfe72fdf98ec
Diane <dhiannbacarrisas13@gmail.com>
2020-12-09 23:28:58 +0800

    Tesda Lesson
    uploading lesson in tesda

---
+++ JS-Lessons/app.js
@@ -0,0 +1,8 @@
+//Log to console
+
+console.log('Hello world');
+console.log(123);
+console.log(true);
+var greetings ='Hello';
+console.log(greetings);
+greetings ='How are you'
---
+++ JS-Lessons/variable.js
@@ -0,0 +1,12 @@
+// var, let, const
+
+var name = 'Diane';
+console.log(name);
+name = 'Rons';
+console.log(name);
+
+
+// Init var
+var greetings;
+greeting ='Hello';
+console.log(greeting);
